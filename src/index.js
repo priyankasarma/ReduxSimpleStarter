239f3181cbc72ae2212cc57441bbb8a0d78c62bc
Priyanka Sarma <priyanka.sarma@3plearning.com>
2017-04-02 15:59:34 +1000

    Video 23 code completed

--- src/index.js
+++ src/index.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+import YTSearch from 'youtube-api-search';
+
 import SearchBar from './components/search-bar';
+import VideoList from './components/video-list';
 
 const API_KEY = 'AIzaSyB9FLYRn6acyI4zTp4cSKbe5A154DHg70M';
 
 // Create a new component that produces some HTML
-const App = () => <div><SearchBar /></div>;
+class App extends Component {
+    constructor(props) {
+        super(props);
 
+        this.state = { videos: [] };
+
+        YTSearch({
+            key: API_KEY,
+            term: 'surfboards'
+        }, videos => {
+            this.setState({ videos });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos={ this.state.videos } />
+            </div>
+        );
+    }
+}
 
 // Render the generated HTML in the DOM of the web page
 ReactDOM.render(<App />, document.querySelector('.container'));
