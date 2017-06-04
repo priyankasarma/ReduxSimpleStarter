e0c764bb6429345d9e4721c1a30912eec7782817
Priyanka Sarma <priyanka.sarma@3plearning.com>
2017-06-04 14:53:24 +1000

    Video 31 code completed

--- src/index.js
+++ src/index.js
@@ -19,9 +19,13 @@ class App extends Component {
             selectedVideo: null
         };
 
+        this.videoSearch('surfboards')
+    }
+
+    videoSearch(term) {
         YTSearch({
             key: API_KEY,
-            term: 'surfboards'
+            term: term
         }, videos => {
             this.setState({
                 videos: videos,
@@ -33,7 +37,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                <SearchBar />
+                <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
                 <VideoDetail video={this.state.selectedVideo}/>
                 <VideoList
                     onVideoSelect={selectedVideo => this.setState({selectedVideo})}
