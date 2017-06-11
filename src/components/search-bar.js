e45fb50145f0c6ba471d70340cacfc5534d3ad72
Priyanka Sarma <priyanka.sarma@3plearning.com>
2017-06-11 13:09:35 +1000

    Updated the default search phrase.

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -4,7 +4,7 @@ class SearchBar extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { term: 'search term' };
+        this.state = { term: 'surfboard' };
     }
 
     render() {
